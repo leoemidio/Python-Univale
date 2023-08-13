08f6f755c585500276dfb5d5f5d0a5d6876023d5
leoemidio <140861064+leoemidio@users.noreply.github.com>
2023-08-13 17:05:18 -0300

    atualização

--- novoarquivo.py
+++ novoarquivo.py
@@ -1,2 +1,13 @@
 print ("123456789")
 # beleza, digitou o códiugo, agr vamos rodar e salvar ele no github 
+# agora voce vai dar os seguintes comandos, pra salvar isso aqui no
+#git hub
+# voce vai digitar o nome do arquivo, e nao usar "git add ."
+#entao fica assim " git add novoarquivo.py"
+#agora voce da um "git commi -a -m" + "qualquermensagem" para atualizar no repositório
+# agora voce vai levar o arquivo para o repositório
+# só dar o comando "git push" 
+
+#viu? o código foi adicionado até o momento que voce deu o "git add"
+#entao quando for na univale, voce primeiro programa e da o git add 
+# so no final
