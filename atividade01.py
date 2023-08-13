537f34e23faf023b8fa30772072acc62d146dd19
leoemidio <140861064+leoemidio@users.noreply.github.com>
2023-08-13 16:41:49 -0300

    Update

---
+++ atividade01.py
@@ -0,0 +1,16 @@
+nome = "Leomar Emidio Vieira"
+idade = 2023 - 1987
+altura = 1,83
+peso = 95
+
+
+print (nome)
+print (str(idade)+"anos")
+print (idade, "anos")
+print (str(altura) + "altura")
+print (str(peso)+ "peso")
+if idade >= 18:
+    print("voce é maior de idade")
+else:
+    print("voce é menor de idade")
+    
---
+++ novoarquivo.py
@@ -0,0 +1,2 @@
+print ("123456789")
+# beleza, digitou o códiugo, agr vamos rodar e salvar ele no github 
