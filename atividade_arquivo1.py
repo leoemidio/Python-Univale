b1469f1cc8e0ac124e261e3a7c4adca88d889eab
leoemidio <140861064+leoemidio@users.noreply.github.com>
2023-09-01 21:52:53 -0300

    01/09.

---
+++ atividade_arquivo1.py
@@ -0,0 +1,3 @@
+from atividade_test import soma
+
+print(soma(2,5))
---
+++ atividade_namemain.py
@@ -0,0 +1,12 @@
+def minha_funcao():
+    return "Função do módulo !"
+
+if __name__ == '__main__':
+   print("Este script está sendo executado sozinho !")
+
+else:
+   print("Este script foi importado!")
+
+
+
+
